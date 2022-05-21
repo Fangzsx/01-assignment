f2d6d4db2ac6569ccedebd56b3d2a84d5782c724
Jay Gilbert Garzon <jygrzn@gmail.com>
2022-05-21 17:25:18 +0800

    add half population

--- 01-assignment/assignment.js
+++ 01-assignment/assignment.js
@@ -4,8 +4,12 @@ let population = 112321181;
 const isIsland = true;
 const language = 'Filipino'
 
+let halfPopulation = population/2;
+
+
 console.log(country);
 console.log(continent);
-console.log(population);
+console.log(population + 1);
+console.log(halfPopulation);
 console.log(isIsland);
 console.log(language);
