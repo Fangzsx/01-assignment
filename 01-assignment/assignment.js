67d7b8ebfbca49dbb58f9ea0315d41b66ae8c86e
Jay Gilbert Garzon <jygrzn@gmail.com>
2022-05-21 17:29:57 +0800

    add description

--- 01-assignment/assignment.js
+++ 01-assignment/assignment.js
@@ -3,8 +3,8 @@ const continent = 'South East Asia';
 let population = 112321181;
 const isIsland = true;
 const language = 'Filipino'
-
-let halfPopulation = population/2;
+let halfPopulation = population / 2;
+let description = country +  ' is in ' + continent + ', and its ' + population + ' people speak ' + language;
 
 
 console.log(country);
@@ -13,3 +13,4 @@ console.log(population + 1);
 console.log(halfPopulation);
 console.log(isIsland);
 console.log(language);
+console.log(description);
