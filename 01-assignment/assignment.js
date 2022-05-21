2e9b8b3da55144a49346b0e75e2a9c35413e07d6
Jay Gilbert Garzon <jygrzn@gmail.com>
2022-05-21 17:19:30 +0800

    add language and isIsland

--- 01-assignment/assignment.js
+++ 01-assignment/assignment.js
@@ -1,7 +1,11 @@
 let country = 'Philippines';
 let continent = 'South East Asia';
 let population = 112321181;
+let isIsland = true;
+let language;
 
 console.log(country);
 console.log(continent);
 console.log(population);
+console.log(isIsland);
+console.log(language);
